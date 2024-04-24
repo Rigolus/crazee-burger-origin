290f6d8b7d31337005e2ed53dba10e74b82b4de8
zakia bouhout <zakia-externe.bouhout@asmeg.org>
2024-04-24 14:17:50 +0200

    :initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Le projet React de Zak.
         </p>
         <a
           className="App-link"
